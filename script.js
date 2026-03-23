a68e1a4837e5837f899cd6de1ea693f8566bbb1a
littleScratcher0911 <luke.rink@icloud.com>
2026-03-23 19:11:34 +0100

    Add greeting and warning messages in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const script_1 = "Willkommen in Luke's Weiterleitung! Danke dass du diese Website besuchst.";
+const script_2 = "Kurzer Hinweis! Diese Website ist NICHT in Google zu finden."
+const beide_scripte = script_1 + " " + script_2;
+console.log(script_1);
+console.log("---------------------------------------");
+console.log(script_2);
+alert(beide_scripte);
